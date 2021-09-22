1a7cfb2a9740868596cfdf5ce5657556241effd5
Mark <mark@example.dev>
2021-09-21 22:23:57 -0700

    improved request handling

--- scrape.py
+++ scrape.py
@@ -1,14 +1,19 @@
 import requests
-from urllib.parse import unquote, urlparse
+from urllib.parse import unquote, urlparse, quote
 from requests.exceptions import ConnectionError
 import os
+from keys import KEY
 
-TOKEN = os.environ.get('OPENGRAPH_API_KEY', 'SECRET_KEY_DEV')
+TOKEN = os.environ.get('OPENGRAPH_API_KEY', KEY)
 
 def opengraphIO_scrape(url:str):
+    print(f'OpenGraph API Call: {url}')
     endpoint = f'https://opengraph.io/api/1.1/site/{url}'
     response = requests.get(endpoint, params={'app_id' : TOKEN}).json()
     return response['hybridGraph']
+
+def incomplete(tags):
+    return (('title' not in tags) or ('description' not in tags) or ('image' not in tags) or ('url' not in tags))
 
 def get_tags(url:str):
     """ Attempts to get the OpenGraph tags of a given URL, using the homebrew request parser. If tags are missing,
@@ -24,31 +29,26 @@ def get_tags(url:str):
 
     try:
         res = requests.get(p_url)
-        if res.status_code == 200:
-            if res.headers['content-type'] in {'image/jpeg', 'image/png', 'image/gif', 'image/webp'}:
-                return {'title' : p_url, 'image' : p_url, 'url' : p_url}
-            elif 'text/html' in res.headers['content-type']:
-                tags = parse_HTML(res.text)
-            else:
-                tags = {'title' : p_url}
+        # Don't look for HTML data on images or non-HTML
+        if res.headers['content-type'] in {'image/jpeg', 'image/png', 'image/gif', 'image/webp'}:
+            return {'title' : p_url, 'image' : p_url, 'url' : p_url}
+        elif 'text/html' in res.headers['content-type']:
+            tags = parse_HTML(res.text)
+            # Make OpenGraph.io API call if we got a good connection but incomplete tags
+            if res.status_code == 200 and incomplete(tags):
+                og_tags = opengraphIO_scrape(quote(p_url, safe=''))
+                print(og_tags)
+                tags['title'] = og_tags.get('title', tags.get('title'))
+                tags['description'] = og_tags.get('description', tags.get('description'))
+                tags['image'] = og_tags.get('image', tags.get('image'))
+                tags['site_name'] = og_tags.get('site_name', tags.get('site_name'))
+                tags['url'] = og_tags.get('url', tags.get('url'))
+            return tags
         else:
-            tags = {'url' : p_url}
+            return {'title' : p_url}
     except ConnectionError:
-        tags = {'url' : p_url}
-    
-    if ('title' not in tags) or ('description' not in tags) or ('image' not in tags) or ('url' not in tags):
-        try:
-            og_tags = opengraphIO_scrape(url)
-            tags['title'] = og_tags['title']
-            tags['description'] = og_tags['description']
-            tags['image'] = og_tags['image']
-            tags['site_name'] = og_tags['site_name']
-            tags['url'] = og_tags['url']
-            return tags
-        except:
-            return tags
-    else:
-        return tags
+        print('Connection Error')
+        return {'url' : p_url, 'description' : 'Sorry, we could not connect to this URL.'}
 
 
 def parse_HTML(content):
